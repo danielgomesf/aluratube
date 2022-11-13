23226f7c862587b6b8e31b0a102759cace920c93
Daniel Gomes Falcao <daniel.falcao@RJ-MB5264.local>
2022-11-13 10:25:53 -0300

    Feature: Supabase Integration

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,44 @@ import Menu from "../src/components/Menu";
 import Timeline from "../src/components/Timeline";
 import Header from "../src/components/Header";
 import Favorites from "../src/components/Favorites";
-
+import { videoService } from "../src/services/videoService";
 
 function HomePage() {
-    const [valorDoFiltro, setValorDoFiltro ] = React.useState("");
+    const service = videoService();
+    const [valorDoFiltro, setValorDoFiltro] = React.useState("");
+    const [playlists, setPlaylists] = React.useState({});
+
+    
+    React.useEffect(() => {
+        service.getAllVideos().then((dados) => {
+            const novasPlaylists = { ...playlists };
+            dados.data.forEach((video) => {
+                if (!novasPlaylists[video.playlist]) {
+                    novasPlaylists[video.playlist] = [];
+                }
+                novasPlaylists[video.playlist] = [
+                    video,
+                    ...novasPlaylists[video.playlist],
+                ];
+            });
+            setPlaylists(novasPlaylists);
+        });
+    }, []);
+
+
     return (
         <>
             <div>
-                <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
+                <Menu
+                    valorDoFiltro={valorDoFiltro}
+                    setValorDoFiltro={setValorDoFiltro}
+                />
                 <Header banner={config.banner} githubPhoto={config.github} />
-                <Timeline  searchValue={valorDoFiltro} playlists={config.playlists} />
-                <Favorites favorites={config.favorites}/>
+                <Timeline searchValue={valorDoFiltro} playlists={playlists} />
+                <Favorites favorites={config.favorites} />
             </div>
         </>
-        
     );
 }
 
-export default HomePage
+export default HomePage;
